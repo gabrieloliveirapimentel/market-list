5b5cc20f2e8b5926687c30224fa29f0233189f61
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-04-15 17:50:17 -0300

    Finalizando o select category

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,45 +1,22 @@
-import { Box, TextField, Autocomplete, Typography } from "@mui/material";
-import { Milk, Beef, Apple, Carrot, Sandwich, Check } from "lucide-react";
+import {
+  Box,
+  TextField,
+  Autocomplete,
+  Typography,
+  Divider,
+} from "@mui/material";
+import {
+  Milk,
+  Beef,
+  Apple,
+  Carrot,
+  Sandwich,
+  Check,
+  ChevronDown,
+} from "lucide-react";
 import { theme } from "../theme/global";
+import { useState } from "react";
 
-export default function CategorySelect() {
-  return (
-    <Autocomplete
-      id="category-select"
-      sx={{ width: 300 }}
-      options={categories}
-      autoHighlight
-      getOptionLabel={(option) => option.label}
-      noOptionsText="Nenhuma categoria encontrada"
-      renderOption={(props, option, { selected }) => {
-        const { key, ...optionProps } = props;
-        return (
-          <Box key={key} component="li" {...optionProps}>
-            {option.icon}
-
-            <Typography
-              sx={{
-                ml: 2,
-                color: theme.palette.primary.main,
-              }}
-              variant="body1"
-            >
-              {option.label}
-            </Typography>
-
-            {selected && (
-              <Check
-                color={theme.palette.primary.main}
-                style={{ position: "absolute", right: 12 }}
-              />
-            )}
-          </Box>
-        );
-      }}
-      renderInput={(params) => <TextField {...params} label="Categoria" />}
-    />
-  );
-}
 interface CategoriesProps {
   id: number;
   label: string;
@@ -75,3 +52,84 @@ const categories: CategoriesProps[] = [
     icon: <Milk color={theme.palette.blue.main} />,
   },
 ];
+
+export default function CategorySelect() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <Autocomplete
+      id="category-select"
+      sx={{
+        width: 300,
+      }}
+      popupIcon={
+        <ChevronDown
+          style={{
+            color: isMenuOpen
+              ? theme.palette.primary.main
+              : theme.palette.gray["gray-200"],
+          }}
+        />
+      }
+      clearIcon={null}
+      options={categories}
+      autoHighlight
+      getOptionLabel={(option) => option.label}
+      noOptionsText="Nenhuma categoria encontrada"
+      onOpen={() => setIsMenuOpen(true)}
+      onClose={() => setIsMenuOpen(false)}
+      slotProps={{
+        paper: {
+          sx: {
+            backgroundColor: theme.palette.gray["gray-400"],
+            border: "2px solid",
+            borderColor: theme.palette.gray["gray-300"],
+            boxShadow: "none",
+            borderRadius: "6px",
+            marginTop: "4px",
+          },
+        },
+      }}
+      renderOption={(props, option, { selected }) => {
+        const { key, ...optionProps } = props;
+        return (
+          <Box>
+            <Box
+              key={key}
+              component="li"
+              {...optionProps}
+              style={{ display: "flex", alignItems: "center", padding: "12px" }}
+            >
+              {option.icon}
+              <Typography
+                sx={{
+                  ml: 2,
+                }}
+                variant="body1"
+              >
+                {option.label}
+              </Typography>
+
+              {selected && (
+                <Check
+                  color={theme.palette.primary.main}
+                  style={{ position: "absolute", right: 12 }}
+                />
+              )}
+            </Box>
+            {key !== "Bebida" && (
+              <Divider
+                component="li"
+                style={{
+                  backgroundColor: theme.palette.gray["gray-300"],
+                  height: "1px",
+                }}
+              />
+            )}
+          </Box>
+        );
+      }}
+      renderInput={(params) => <TextField {...params} label="Categoria" />}
+    />
+  );
+}
