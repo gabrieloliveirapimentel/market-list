11fa33579c0ac7107227bfb331e15e23b81f13f0
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-04-23 17:31:39 -0300

    feat: Ajustando a App page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,94 @@
-import { ThemeProvider, Typography } from "@mui/material";
-import { ButtonUsage } from "./components/button";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { theme } from "./theme/global";
-import CategorySelect from "./components/select";
+import { ListCardItem } from "./components/card";
+import { useState } from "react";
+import { Header } from "./components/header";
+import { Grid } from "@mui/material";
+import { FormUsage } from "./components/form/form";
+
+interface ItemProps {
+  id: string;
+  label: string;
+  category: string;
+  quantity: number;
+  unit: string;
+  isChecked: boolean;
+}
 
 export function App() {
+  const [items, setItems] = useState<ItemProps[]>([
+    {
+      id: "1",
+      label: "Pão francês",
+      category: "Padaria",
+      quantity: 1,
+      unit: "unidade",
+      isChecked: false,
+    },
+    {
+      id: "2",
+      label: "Maçã",
+      category: "Fruta",
+      quantity: 3,
+      unit: "unidades",
+      isChecked: false,
+    },
+    {
+      id: "3",
+      label: "Leite",
+      category: "Bebida",
+      quantity: 2,
+      unit: "litros",
+      isChecked: false,
+    },
+  ]);
+
+  function handleCheckItem(id: string) {
+    const updatedItems = items.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          isChecked: !item.isChecked,
+        };
+      }
+      return item;
+    });
+    setItems(updatedItems);
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Typography variant="h5">App page!</Typography>
-      <ButtonUsage />
-      <CategorySelect />
+      <CssBaseline />
+      <Header />
+
+      <Container>
+        <FormUsage />
+        <Grid container spacing={2}>
+          {items.length > 0 ? (
+            items.map((item) => (
+              <ListCardItem
+                key={item.id}
+                id={item.id}
+                label={item.label}
+                category={item.category}
+                quantity={item.quantity}
+                unit={item.unit}
+                isChecked={item.isChecked}
+                setIsChecked={handleCheckItem}
+              />
+            ))
+          ) : (
+            <Typography variant="body1" color={theme.palette.gray["gray-200"]}>
+              Nenhum item encontrado
+            </Typography>
+          )}
+        </Grid>
+      </Container>
     </ThemeProvider>
   );
 }
