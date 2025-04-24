ca3c5693f1a194455ec18291047ad9c0f6d0bcda
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-04-24 17:44:04 -0300

    style: Ajuste dos estilos globais

--- src/theme/global.tsx
+++ src/theme/global.tsx
@@ -1,6 +1,6 @@
 import { createTheme } from "@mui/material";
 
-const base = createTheme({
+const base = {
   palette: {
     primary: {
       main: "#7450AC",
@@ -44,10 +44,33 @@ const base = createTheme({
   typography: {
     fontFamily: `'Inter', sans-serif`,
   },
-});
+};
+
+const shared = {
+  backgroundColor: base.palette.gray["gray-400"],
+
+  "&.Mui-focused fieldset": {
+    borderColor: base.palette.primary.main,
+  },
+  "&:hover fieldset": {
+    border: `2px solid ${base.palette.primary.main}`,
+  },
+};
+
+const sharedBorder = {
+  border: `2px solid ${base.palette.gray["gray-300"]}`,
+};
 
 export const theme = createTheme(base, {
   components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: base.palette.gray["gray-600"],
+          color: base.palette.gray["gray-100"],
+        },
+      },
+    },
     MuiTypography: {
       styleOverrides: {
         root: {
@@ -55,35 +78,17 @@ export const theme = createTheme(base, {
         },
       },
     },
-    MuiInputBase: {
-      styleOverrides: {
-        input: {
-          color: base.palette.gray["gray-200"],
-        },
-      },
-    },
     MuiAutocomplete: {
       styleOverrides: {
         root: {
-          "& .MuiOutlinedInput-root": {
-            borderRadius: "8px",
-            backgroundColor: base.palette.gray["gray-400"],
-            "& fieldset": {
-              border: "2px solid",
-              borderColor: base.palette.gray["gray-300"],
-            },
-            "&:hover fieldset": {
-              border: "2px solid",
-              borderColor: base.palette.primary.main,
-            },
-            "&.Mui-focused fieldset": {
-              borderColor: base.palette.primary.main,
-            },
-          },
-          "& .MuiInputLabel-root": {
-            color: base.palette.gray["gray-200"],
-            "&.Mui-focused": {
-              color: base.palette.primary.main,
+          "&.category-select": {
+            "& .MuiOutlinedInput-root": {
+              ...shared,
+              borderRadius: "8px",
+
+              "& fieldset": {
+                ...sharedBorder,
+              },
             },
           },
         },
@@ -92,35 +97,45 @@ export const theme = createTheme(base, {
     MuiTextField: {
       styleOverrides: {
         root: {
-          "& .MuiOutlinedInput-root": {
+          "&.input-base": {
+            ...shared,
             borderRadius: "8px",
-            backgroundColor: base.palette.gray["gray-400"],
             "& fieldset": {
-              border: "2px solid",
-              borderColor: base.palette.gray["gray-300"],
-            },
-            "&:hover fieldset": {
-              border: "2px solid",
-              borderColor: base.palette.primary.main,
-            },
-            "&.Mui-focused fieldset": {
-              borderColor: base.palette.primary.main,
+              borderRadius: "8px",
+              ...sharedBorder,
             },
           },
-          "& .MuiInputLabel-root": {
-            color: base.palette.gray["gray-200"],
-            "&.Mui-focused": {
-              color: base.palette.primary.main,
+          "&.input-quantity": {
+            ...shared,
+            borderRadius: "8px 0px 0px 8px",
+
+            "& fieldset": {
+              zIndex: 2,
+              borderRadius: "8px 0px 0px 8px",
+              ...sharedBorder,
             },
           },
         },
       },
     },
-    MuiCssBaseline: {
+    MuiInputBase: {
       styleOverrides: {
-        body: {
-          backgroundColor: base.palette.gray["gray-600"],
-          color: base.palette.gray["gray-100"],
+        root: {
+          color: base.palette.gray["gray-200"],
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "&.select-quantity": {
+            ...shared,
+            borderRadius: "0px 8px 8px 0px",
+            marginLeft: "-2px",
+            "& fieldset": {
+              ...sharedBorder,
+            },
+          },
         },
       },
     },
